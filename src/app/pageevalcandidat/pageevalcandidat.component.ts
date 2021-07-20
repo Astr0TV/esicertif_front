b74e5bc68081445fbe7b458c1db6e1563fea8bcd
mariem548 <mariembensalah131@gmail.com>
2021-07-20 22:08:32 +0200

    correction de formulaire

--- src/app/pageevalcandidat/pageevalcandidat.component.ts
+++ src/app/pageevalcandidat/pageevalcandidat.component.ts
@@ -13,14 +13,14 @@ import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
 export class PageevalcandidatComponent implements OnInit {
   firstFormGroup: FormGroup;
   firstFormGroup2:FormGroup;
-  secondFormGroup: FormGroup;
-  secondFormGroup2: FormGroup;
+  secondFormGroup: FormGroup;secondFormGroup2: FormGroup;
   isEditable = false;
   isLinear = false;
   reponse :any; 
   reponse2 :any;
   reponse3:any;
   reponse4:any;
+ 
   constructor(private _formBuilder: FormBuilder) {}
 
   ngOnInit() {
@@ -38,13 +38,15 @@ export class PageevalcandidatComponent implements OnInit {
     this.secondFormGroup2 = this._formBuilder.group({
       secondCtrl: ['', Validators.required]
     });
+   
   
   }
 
   favoritereponse: string;
   favoritereponse2: string;
-  favoritereponse3: string;
-  favoritereponse4:string;
+  favoritereponse3: string;  
+  favoritereponse4: string;
+
 
   reponses: string[] = ['Pas du tout', 'Insuffisamment', 'En partie', 'Totalement'];
   
