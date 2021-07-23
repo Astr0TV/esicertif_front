bed38f88e3380cff683b0dce8aadc91db41f14a8
Astr0TV <lamsaouriismail@gmail.com>
2021-07-23 11:13:45 +0200

    formateur

--- src/app/candidatformateur/candidatformateur.component.ts
+++ src/app/candidatformateur/candidatformateur.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ConnexionService } from '../service/connexion.service';
 
 @Component({
   selector: 'app-candidatformateur',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./candidatformateur.component.css']
 })
 export class CandidatformateurComponent implements OnInit {
+  formation: any;
+connexionnew: any;
 
-  constructor() { }
+  constructor(private http: HttpClient,private route: Router,private connexion: ConnexionService) { }
+
+  deconnect(): any {
+    localStorage.clear();
+    this.route.navigateByUrl('/accueil');
+  }
 
   ngOnInit(): void {
+    var test = JSON.parse(localStorage.getItem('userConnect') || '{}' ) 
+    this.http.get('http://localhost:8089/user/'+ test.id).subscribe({
+      next: (data) => { this.connexionnew = data; 
+        console.log('this msg concernec les informations de'); 
+        console.log(data) },
+      error: (err) => {console.log(err); }
+    });
+    this.http.get('http://localhost:8089/formation/formateur/'+ test.id).subscribe({
+      next: (data) => { this.formation = data; 
+        console.log('this msg concernec les informations de'); 
+        console.log(data) },
+      error: (err) => {console.log(err); }
+    });
   }
 
 }
